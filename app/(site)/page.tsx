c1abf5c928f15ac2aafd016c9d85c13b948659ab
hqchqc <734986933@qq.com>
2023-06-03 17:02:17 +0800

    feat: 登录注册界面

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,3 +1,6 @@
+import Image from 'next/image'
+import AuthForm from './components/AuthForm'
+
 export default function Home() {
   return (
     <div 
@@ -12,7 +15,28 @@ export default function Home() {
         bg-gray-100
       "
     >
-      <div className="sm:mx-auto sm:w-full sm:max-w-md"></div>
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
+        <Image 
+          alt="Logo" 
+          height="48" 
+          width="48" 
+          className='mx-auto w-auto' 
+          src="/images/logo.png" 
+        />
+        <h2
+          className='
+            mt-6
+            text-center
+            text-3xl
+            font-bold
+            tracking-tight
+            text-gray-900
+          '>
+            Sign in to your account
+          </h2>
+
+          <AuthForm />
+      </div>
     </div>
   )
 }
